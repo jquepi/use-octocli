d844ff685b8320a48f7add16536a6a2c49602aa4
Michael Levan <mlevan1992@gmail.com>
2020-10-23 11:33:41 -0400

    Initial commit for use-octocli

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	octocli()
+}
+
+func octocli() []string {
+	var arg []string
+	var optionalArgs []string
+
+	arg = append(arg, "--project=${INPUT_PROJECT}")
+	arg = append(arg, "--packageVersion=${INPUT_PACKAGE_VERSION}")
+	arg = append(arg, "--server=${INPUT_OCTOPUS_SERVER}")
+	arg = append(arg, "--apikey=${API_KEY}")
+
+	if set == true {
+		optionalArgs = append(arg, "--space=${INPUT_SPACE}")
+		return optionalArgs
+	}
+
+	return []string{""}
+}
